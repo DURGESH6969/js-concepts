f499672dc20b1da6d11b6f97c7fba4391f56ae33
Durgesh Gupta <78210938+DURGESH6969@users.noreply.github.com>
2023-12-23 06:35:06 +0000

    added  interview pov qna

---
+++ 02_classes_and_oop/execution-context.js
@@ -0,0 +1,11 @@
+// javascript is a single threaded , high - level , interpreted, object - oriented scripting language primarily used for front-end web development. It is a crucial component of web technologies, enabling dynamic and interactive user experiences. 
+
+// Most Important Note - "Everything in javascript happens inside an Execution Context"
+
+// Execution context is like a big box . It has two component, the first one is memory component - it is the place where all the variables and functions are stored as key value pair ex - const a = 10; will be stored like this a : 10, functions are also stored inside this memory component. Memory component is also known as Variable Enviroment.
+
+// The second component of the execution context is the code component. In  this code component, every single line of code is executed one line at a time. This code component is also known as Thread of Execution. 
+
+// Another Important Note - "Javascript is a synchronous single-threaded language."
+
+// J
